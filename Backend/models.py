f9e8ebc415ee32914b948c8dc48193a5ffbd1da7
Konkona Das <konkona.das.16@gmail.com>
2026-07-29 10:38:12 +0530

    changed_tables

--- Backend/models.py
+++ Backend/models.py
@@ -2,14 +2,14 @@ from sqlalchemy import Column, Integer, String, Text
 from pymysql import Timestamp
 from database import Base
 
-class City(Base):
+'''class City(Base):
     __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True, index=True)
     city_name = Column(String(100), nullable=False)
     state = Column(String(100), nullable=False)
     population = Column(Integer)
-    famous_place = Column(String(200))
+    famous_place = Column(String(200))'''
 
 
 class Area(Base):
@@ -20,33 +20,54 @@ class Area(Base):
     latitude = Column(Integer)
     longitude = Column(Integer)
 
-class Accident(Base):
-    __tablename__ = "accident_history"
+class Weather(Base):
+    __tablename__ = "weather_data"
 
     id = Column(Integer, primary_key=True, index=True)
-    road_id = Column(Text, nullable=False)
-    severity = Column(Text)
+    area = Column(Text, nullable=False)
+    temperature = Column(Integer)
+    humidity = Column(Integer)
+    windspeed = Column(Integer)
+    rainfall=Column(Integer)
     timestamp = Column(Timestamp)
 
-    __tablename__ = "accident_risk_predictions"
-    
+
+class Pollution(Base):
+    __tablename__ = "pollution_data"
+
     id = Column(Integer, primary_key=True, index=True)
-    road_id = Column(Text, nullable=False)
-    risk_probability = Column(float)
+    area = Column(String(100), nullable=False)
+    aqi = Column(Integer)
+    pm2_5 = Column(Integer)
+    pm10 = Column(Integer)
+    timestamp = Column(Timestamp)
+
+
+    __tablename__ = "pollution_predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    area = Column(String(100), nullable=False)
+    predicted_aqi = Column(Integer)
+    risk_level = Column(Timestamp)
+    predicted_for = Column(Timestamp)
     generated_at = Column(Timestamp)
 
-class Energy(Base):
-    __tablename__ = "energy_data"
-
+class Traffic(Base):
+    __tablename__ = "traffic_data"
+    
     id = Column(Integer, primary_key=True, index=True)
-    area = Column(String(100), nullable=False)
-    power_usage_kwh = Column(Integer)
-    timestamp = Column(Integer)
-
-
-    __tablename__ = "energy_predictions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    area = Column(String(100), nullable=False)
-    power_usage_kwh = Column(Integer)
+    road_id = Column(String(100), nullable=False)
+    area = Column(Integer)
+    vehicle_count = Column(Integer)
+    average_speed=Column(Integer)
     timestamp = Column(Timestamp)
+
+
+    __tablename__ = "traffic_predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    road_id = Column(String(100), nullable=False)
+    predicted_vehicle_count = Column(Integer)
+    predicted_status=Column(String(100))
+    predicted_for = Column(Timestamp)
+    generated_at = Column(Timestamp)
